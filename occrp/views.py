4fb2fb58bb4c3db8efb2481e224dd874c2d3fe71
reginafcompton <reginafcompton@gmail.com>
2017-06-29 12:30:42 -0500

    Add form for creating a new story

--- occrp/views.py
+++ occrp/views.py
@@ -1,13 +1,28 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 
-from .models import Person
-from .forms import PersonForm
+from .models import Story, Person
+from .forms import StoryForm, PersonForm
 from .database import db
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/', methods=['GET', 'POST'])
 def index():
+    form = StoryForm()
+
+    if form.validate_on_submit():
+      title = form.data['title']
+
+      story = Story(title=title)
+      db.session.add(story)
+      db.session.commit()
+
+    return render_template('index.html', form=form)
+
+
+@views.route('/story/<story_id>', methods=['GET', 'POST'])
+def story(story_id):
     form = PersonForm()
 
     if form.validate_on_submit():
@@ -19,9 +34,9 @@ def index():
 
         flash('Person {} saved!'.format(person.name))
     
-    return render_template('index.html', form=form)
+    return render_template('story.html', form=form)
 
-@views.route('/about/')
+
+@views.route('/about')
 def about():
     return render_template('about.html')
-
