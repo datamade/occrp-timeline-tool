fb17e1e6cbfacce5e0c00909cc5bc38eff25f360
reginafcompton <reginafcompton@gmail.com>
2017-08-08 10:06:59 -0500

    Add JS binding for toggling of icons, and improve UI for events table

--- occrp/static/js/custom.js
+++ occrp/static/js/custom.js
@@ -1,34 +1,40 @@
 $(document).ready(function() {
-    // $('.collapse').collapse()
-
-    // Todo: not working?
-    $("body").on("mouseover", "tr.detailsControl", function(e){
-        $(this).css("background-color", "red")
+    // Facets panel
+    $('.panel-show').addClass('in');
+    $('.panel-heading').on('click', function () {
+        if ($(this).parent().children().eq(1).hasClass('in')){
+            $(this).find('i').removeClass('fa-minus').addClass('fa-plus');
+        } else{
+            $(this).find('i').removeClass('fa-plus').addClass('fa-minus');
+        }
     });
 
-    $("body").on("mouseout", "tr.detailsControl", function(e){
-        $(this).css("background-color", "#eee")
+    // Events table
+    $('body').on('mouseover', 'tr.detailsControl', function(e){
+        $(this).css('background-color', '#f9f9f9')
     });
 
-    $("body").on("click", "tr.detailsControl", function(e){
+    $('body').on('mouseout', 'tr.detailsControl', function(e){
+        $(this).css('background-color', '#fff')
+    });
+
+    $('body').on('click', 'tr.detailsControl', function(e){
         e.stopPropagation();
-
-        var target = $(e.target);
-
+        target = $(e.target);
         target.closest('tr').next('tr').children().slideToggle(100);
 
-        // var right = $(this).find('.fa-chevron-circle-right');
-        // var down = $(this).find('.fa-chevron-circle-down');
+        var right = $(this).find('.fa-chevron-circle-right');
+        var down = $(this).find('.fa-chevron-circle-down');
 
-        // if(($(right).is(":visible"))) {
-        //     $(this).find('.fa-chevron-circle-down').show();
-        //     $(this).find('.fa-chevron-circle-right').hide();
-        //     $(this).css("font-weight", "700")
-        // } else {
-        //     $(this).find('.fa-chevron-circle-down').hide();
-        //     $(this).find('.fa-chevron-circle-right').show();
-        //     $(this).css("font-weight", "400")
-        // }
+        if(($(right).is(":visible"))) {
+            $(this).find('.fa-chevron-circle-down').show();
+            $(this).find('.fa-chevron-circle-right').hide();
+            $(this).css("font-weight", "700");
+        } else {
+            $(this).find('.fa-chevron-circle-down').hide();
+            $(this).find('.fa-chevron-circle-right').show();
+            $(this).css("font-weight", "400");
+        }
     });
 
 });
